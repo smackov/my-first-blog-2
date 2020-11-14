13586f190b45442531c961f8d5076dafc817e549
smackov <smackovdinislam@gmail.com>
2020-11-14 13:54:48 +0500

    DEBUG in settings.py was changed.

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v!hg3x$m2q8-+j2(8$0w09qpww7uiy4pofcjngg@wcc2%klj^c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '.pythonanywhere.com']
 
